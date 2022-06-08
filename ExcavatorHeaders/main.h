469d0f9761e9ec5dc78c1aa19a58cd065e18d568
sarki012 <60797267+sarki012@users.noreply.github.com>
2022-06-08 10:17:05 -0500

    main.h

---
+++ ExcavatorHeaders/main.h
@@ -0,0 +1,24 @@
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef MAIN_H
+#define	MAIN_H
+
+#include <xc.h> // include processor files - each processor file is guarded. 
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "croutine.h"
+#include "FreeRTOSConfig.h"
+
+#define SAMPLE_RATE 20      //Was 20
+#define LOOPS 2000      //Tried 2000. Too fast. Last try: 2500. Ok but still too fast. Last try 3000
+#define LOOPS_CURL 1500     //Want the curl to be faster
+void init(void);
+void leftThread(void *pvParameters);
+void rightThread(void *pvParameters);
+void spinThread(void *pvParameters);
+int charToInt(char digit2, char digit1, char digit0);
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
